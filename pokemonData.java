d1b0a9845c58b4ed4e0a570a0ae3a90583ed05fd
tamcl <37628162+tamcl@users.noreply.github.com>
2019-04-20 22:17:40 +0100

    nothing much

--- pokemonData.java
+++ pokemonData.java
@@ -3,20 +3,22 @@ import java.util.*;
 
 
 public class pokemonData{
-	public ArrayList<String> pokemon = new ArrayList<String>();
+	public HashMap<Integer,String> pokemon = new HashMap<Integer,String>();
+	public int count = 0;
 	public pokemonData(){
 	
 	}
-	public void add(String Name){
-		pokemon.add(Name);
-		System.out.println(Name);
+	public void add(int no, String Name){
+		pokemon.put(no,Name);
+		count++;
+		//System.out.println(no+"\t"+Name + "\t\t"+count);
 	}
 	public void print(){
-		for(String a:pokemon){
-		System.out.print(a+", ");
+		for(int i=1;i<808;i++){
+			System.out.println(pokemon.get(i));
 		}
 	}
 	public void number(){
-		System.out.println(pokemon.size());
+		System.out.println(count);
 	}
 }
